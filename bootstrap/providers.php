579cb627ff13cca3e9a72e8dbeec0a828583100d
Aleksandar Djukic <djukicaleks91@gmail.com>
2026-02-16 20:45:38 +0100

    refactor(modules): auto-discover module service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,17 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Modules\Auth\Providers\AuthModuleServiceProvider::class,
-    App\Modules\Event\Providers\EventModuleServiceProvider::class,
-    App\Modules\Ticket\Providers\TicketModuleServiceProvider::class,
-    App\Modules\Booking\Providers\BookingModuleServiceProvider::class,
-    App\Modules\Payment\Providers\PaymentModuleServiceProvider::class,
-];
+$moduleProviders = [];
+
+foreach (glob(app_path('Modules/*/Providers/*ModuleServiceProvider.php')) ?: [] as $providerFile) {
+    $relativePath = str_replace([app_path().DIRECTORY_SEPARATOR, '.php'], '', $providerFile);
+    $moduleProviders[] = 'App\\'.str_replace(DIRECTORY_SEPARATOR, '\\', $relativePath);
+}
+
+sort($moduleProviders);
+
+return array_merge(
+    [
+        App\Providers\AppServiceProvider::class,
+    ],
+    $moduleProviders
+);
